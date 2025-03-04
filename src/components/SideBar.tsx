c414a0face875f6a16cbb770dbcdb8a33aa65295
Greatnexx <godwindanielgodwin@gmail.com>
2025-03-04 15:33:33 +0100

    initial commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,40 @@
+
+const SideBar = () => {
+  return (
+    <div className="mt-20 space-y-3">
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[20px] rounded-[9px] w-[303px] text-center">Nafada</button>
+
+        </div>
+       <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Dukku</button>
+       </div>
+       <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Funakaye</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Kwami</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Gombe</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Yamaltu/Deba</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Akko</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Billiri</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Balanga</button>
+       </div>
+        <div>
+         <button className="border border-[#CBD5E0] py-[12px] px-[14px] rounded-[9px]  w-[303px] text-center">Kaltungo</button>
+       </div>
+    </div>
+  )
+}
+
+export default SideBar
